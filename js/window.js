bc9cd1c2df6afaa4f54297bf67e3a9690c8465dd
Airdust <51649624+Airdust123@users.noreply.github.com>
2021-01-25 10:21:59 +0800

    Add files via upload

---
+++ js/window.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+  let hour = new Date().getHours()
+  let str = ''
+  if (hour < 6) {
+    str = '凌晨'
+  } else if (hour < 9) {
+    str = '早上'
+  } else if (hour < 12) {
+    str = '上午'
+  } else if (hour < 14) {
+    str = '中午'
+  } else if (hour < 17) {
+    str = '下午'
+  } else if (hour < 19) {
+    str = '傍晚'
+  } else if (hour < 22) {
+    str = '晚上'
+  } else {
+    str = '夜里'
+  }
+ 
+  function c () {
+    document.title = document[a] ? '🎉 Hi,访问微尘的小屋的小伙伴,' + str + '好! [' + d + ']' : d
+  }
+ 
+  let a = document.title
+  let b = document.title
+  let d = document.title
+  typeof document.hidden !== 'undefined' ? (a = 'hidden', b = 'visibilitychange')
+    : typeof document.mozHidden !== 'undefined' ? (a = 'mozHidden', b = 'mozvisibilitychange')
+    : typeof document.webkitHidden !== 'undefined' && (a = 'webkitHidden', b = 'webkitvisibilitychange')
+  typeof document.addEventListener === 'undefined' && typeof document[a] === 'undefined' || document.addEventListener(b, c, !1)
+}
